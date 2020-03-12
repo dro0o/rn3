1a3e5a79fc8569a847f76a2f522234c4a94c3dae
Andrew Pittman <andrew@pittman.dev>
2020-03-11 22:32:46 -0400

    crazy global state automation

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,22 +1,24 @@
-import React, { useState } from 'react'
+import createDataContext from './createDataContext'
 
-const BlogContext = React.createContext()
-
-export const BlogProvider = ({ children }) => {
-  const [blogPosts, setBlogPosts] = useState([])
-
-  const addBlogPost = () => {
-    setBlogPosts([
-      ...blogPosts,
-      { title: `Blog Post #${blogPosts.length + 1}` }
-    ])
+const blogReducer = (state, action) => {
+  switch (action.type) {
+    case 'add_blogpost':
+      return [...state, { title: `Blog Post #${state.length + 1}` }]
+    case 'edit_blogpost':
+    case 'delete_blogpost':
+    default:
+      return state
   }
-
-  return (
-    <BlogContext.Provider value={{ data: blogPosts, addBlogPost: addBlogPost }}>
-      {children}
-    </BlogContext.Provider>
-  )
 }
 
-export default BlogContext
+const addBlogPost = dispatch => {
+  return () => {
+    dispatch({ type: 'add_blogpost' })
+  }
+}
+
+export const { Context, Provider } = createDataContext(
+  blogReducer,
+  { addBlogPost },
+  []
+)
